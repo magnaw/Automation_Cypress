0ff104a31c27390a5d54ce2febbe0a922160d1ba
magnaw <magnaou@we-plus.fr>
2023-12-13 09:53:01 +0100

    Third commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     watchForFileChanges:false,
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       // implement node event listeners here
     },
   },
